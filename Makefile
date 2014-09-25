5a791903a6e33a7a0938297ae035a9033033254f
guybedford <guybedford@gmail.com>
2014-09-25 22:26:30 +0200

    rename SystemProductionCSP to SystemCSP

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@ START = cat lib/banner.js lib/polyfill-wrapper-start.js > dist/$@.js;
 END = cat lib/polyfill-wrapper-end.js >> dist/$@.js;
 
 SystemJS = meta register core global cjs amd map plugins bundles versions depCache
-SystemProductionCSP = scriptLoader meta register core global cjs amd map plugins bundles versions depCache
+SystemCSP = scriptLoader meta register core global cjs amd map plugins bundles versions depCache
 
-all: system system-production-csp uglify
+all: system system-csp uglify
 
 uglify:
 	cat lib/banner.js > dist/system.min.js;
 	./node_modules/.bin/uglifyjs dist/system.js -cm >> dist/system.min.js
 
-	cat lib/banner.js > dist/system-production-csp.min.js;
-	./node_modules/.bin/uglifyjs dist/system-production-csp.js -cm >> dist/system-production-csp.min.js
+	cat lib/banner.js > dist/system-csp.min.js;
+	./node_modules/.bin/uglifyjs dist/system-csp.js -cm >> dist/system-csp.min.js
 
 
 system:
@@ -26,12 +26,12 @@ system:
 	done
 	$(END)
 
-system-production-csp:
+system-csp:
 	$(START)
-	for extension in $(SystemProductionCSP); do \
+	for extension in $(SystemCSP); do \
 		cat lib/extension-$$extension.js >> dist/$@.js; \
 	done
-	for extension in $(SystemProductionCSP); do \
+	for extension in $(SystemCSP); do \
 		echo $$extension"(System);" >> dist/$@.js; \
 	done
 	$(END)
--- dist/system-csp.js
+++ dist/system-csp.js
@@ -1,5 +1,5 @@
 /*
- * SystemJS v0.8.1
+ * SystemJS v0.8.2
  */
 
 (function($__global) {
@@ -100,6 +100,10 @@ function scriptLoader(loader) {
         // we now need to tell the wrapper handlers that
         // this load record has just executed
         loader.onScriptLoad(load);
+
+        // if nothing registered, then something went wrong
+        if (!load.metadata.registered)
+          reject(load.address + ' did not call System.register or AMD define');
 
         resolve('');
       }
@@ -397,10 +401,10 @@ function register(loader) {
       if (anonRegister)
         load.metadata.entry = anonRegister;
       
-      if (anonRegister || calledRegister)
+      if (calledRegister) {
         load.metadata.format = load.metadata.format || 'register';
-      if (calledRegister)
         load.metadata.registered = true;
+      }
     }
   }
 
@@ -943,6 +947,14 @@ function core(loader) {
   See the SystemJS readme global support section for further information.
 */
 function global(loader) {
+
+  function readGlobalProperty(p, value) {
+    var pParts = p.split('.');
+    while (pParts.length)
+      value = value[pParts.shift()];
+    return value;
+  }
+
   function createHelpers(loader) {
     if (loader.has('@@global-helpers'))
       return;
@@ -966,7 +978,10 @@ function global(loader) {
         // now store a complete copy of the global object
         // in order to detect changes
         curGlobalObj = {};
-        ignoredGlobalProps = ['indexedDB', 'sessionStorage', 'localStorage', 'clipboardData', 'frames', 'webkitStorageInfo'];
+        ignoredGlobalProps = ['indexedDB', 'sessionStorage', 'localStorage',
+          'clipboardData', 'frames', 'webkitStorageInfo', 'toolbar', 'statusbar',
+          'scrollbars', 'personalbar', 'menubar', 'locationbar', 'webkitIndexedDB'
+        ];
         for (var g in loader.global) {
           if (indexOf.call(ignoredGlobalProps, g) != -1) { continue; }
           if (!hasOwnProperty || loader.global.hasOwnProperty(g)) {
@@ -996,7 +1011,7 @@ function global(loader) {
         // if one global, then that is the module directly
         else if (exportName) {
           var firstPart = exportName.split('.')[0];
-          singleGlobal = eval.call(loader.global, exportName);
+          singleGlobal = readGlobalProperty(exportName, loader.global);
           exports[firstPart] = loader.global[firstPart];
         }
 
@@ -1071,12 +1086,12 @@ function cjs(loader) {
 
   // CJS Module Format
   // require('...') || exports[''] = ... || exports.asd = ... || module.exports = ...
-  var cjsExportsRegEx = /(?:^\s*|[}{\(\);,\n=:\?\&]\s*|module\.)(exports\s*\[\s*('[^']+'|"[^"]+")\s*\]|\exports\s*\.\s*[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*|exports\s*\=)/;
-  var cjsRequireRegEx = /(?:^\s*|[}{\(\);,\n=:\?\&]\s*)require\s*\(\s*("([^"]+)"|'([^']+)')\s*\)/g;
+  var cjsExportsRegEx = /(?:^|[^$_a-zA-Z\xA0-\uFFFF.]|module\.)(exports\s*\[['"]|\exports\s*\.)|(?:^|[^$_a-zA-Z\xA0-\uFFFF.])module\.exports\s*\=/;
+  // RegEx adjusted from https://github.com/jbrantly/yabble/blob/master/lib/yabble.js#L339
+  var cjsRequireRegEx = /(?:^|[^$_a-zA-Z\xA0-\uFFFF.])require\s*\(\s*("[^"\\]*(?:\\.[^"\\]*)*"|'[^'\\]*(?:\\.[^'\\]*)*')\s*\)/g;
   var commentRegEx = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
 
   function getCJSDeps(source) {
-    cjsExportsRegEx.lastIndex = 0;
     cjsRequireRegEx.lastIndex = 0;
 
     var deps = [];
@@ -1087,7 +1102,7 @@ function cjs(loader) {
     var match;
 
     while (match = cjsRequireRegEx.exec(source))
-      deps.push(match[2] || match[3]);
+      deps.push(match[1].substr(1, match[1].length - 2));
 
     return deps;
   }
@@ -1154,10 +1169,10 @@ function amd(loader) {
   // AMD Module Format Detection RegEx
   // define([.., .., ..], ...)
   // define(varName); || define(function(require, exports) {}); || define({})
-  var amdRegEx = /(?:^\s*|[}{\(\);,\n\?\&]\s*)define\s*\(\s*("[^"]+"\s*,\s*|'[^']+'\s*,\s*)?\s*(\[(\s*(("[^"]+"|'[^']+')\s*,|\/\/.*\r?\n|\/\*(.|\s)*?\*\/))*(\s*("[^"]+"|'[^']+')\s*,?)?(\s*(\/\/.*\r?\n|\/\*(.|\s)*?\*\/))*\s*\]|function\s*|{|[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*\))/;
+  var amdRegEx = /(?:^|[^$_a-zA-Z\xA0-\uFFFF.])define\s*\(\s*("[^"]+"\s*,\s*|'[^']+'\s*,\s*)?\s*(\[(\s*(("[^"]+"|'[^']+')\s*,|\/\/.*\r?\n|\/\*(.|\s)*?\*\/))*(\s*("[^"]+"|'[^']+')\s*,?)?(\s*(\/\/.*\r?\n|\/\*(.|\s)*?\*\/))*\s*\]|function\s*|{|[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*\))/;
   var commentRegEx = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
 
-  var cjsRequirePre = "(?:^\\s*|[}{\\(\\);,\\n=:\\?\\&]\\s*)";
+  var cjsRequirePre = "(?:^|[^$_a-zA-Z\\xA0-\\uFFFF.])";
   var cjsRequirePost = "\\s*\\(\\s*(\"([^\"]+)\"|'([^']+)')\\s*\\)";
 
   var fnBracketRegEx = /\(([^\)]*)\)/;
@@ -1234,8 +1249,10 @@ function amd(loader) {
     var onScriptLoad = loader.onScriptLoad;
     loader.onScriptLoad = function(load) {
       onScriptLoad(load);
-      if (anonDefine || defineBundle)
+      if (anonDefine || defineBundle) {
         load.metadata.format = 'defined';
+        load.metadata.registered = true;
+      }
 
       if (anonDefine) {
         load.metadata.deps = load.metadata.deps ? load.metadata.deps.concat(anonDefine.deps) : anonDefine.deps;
@@ -1400,30 +1417,12 @@ function amd(loader) {
     var loader = this;
 
     if (load.metadata.format == 'amd' || !load.metadata.format && load.source.match(amdRegEx)) {
+      console.log('is amd');
       load.metadata.format = 'amd';
 
       createDefine(loader);
 
-      try {
-        loader.__exec(load);
-      }
-      catch(e) {
-        if (loader.execute === false && isNode) {
-          // use a regular expression to pull out deps
-          var match = load.source.match(amdRegEx);
-          if (match) {
-            // named or anonymous
-            if (match[1] && match[1][0] == '[')
-              define(match[1].substr(match[1].length - 2), eval(match[2]), function() {});
-            else if (match[2] && match[2][0] == '[')
-              define(eval(match[2]), function() {});
-            else
-              define(function() {});
-          }
-        }
-        else
-          throw e;
-      }
+      loader.__exec(load);
 
       removeDefine(loader);
 
@@ -1592,6 +1591,157 @@ function map(loader) {
   }
 }
 /*
+  SystemJS Plugin Support
+
+  Supports plugin syntax with "!"
+
+  The plugin name is loaded as a module itself, and can override standard loader hooks
+  for the plugin resource. See the plugin section of the systemjs readme.
+*/
+function plugins(loader) {
+  if (typeof indexOf == 'undefined')
+    indexOf = Array.prototype.indexOf;
+
+  var loaderNormalize = loader.normalize;
+  loader.normalize = function(name, parentName, parentAddress) {
+    var loader = this;
+    // if parent is a plugin, normalize against the parent plugin argument only
+    var parentPluginIndex;
+    if (parentName && (parentPluginIndex = parentName.indexOf('!')) != -1)
+      parentName = parentName.substr(0, parentPluginIndex);
+
+    return Promise.resolve(loaderNormalize.call(loader, name, parentName, parentAddress))
+    .then(function(name) {
+      // if this is a plugin, normalize the plugin name and the argument
+      var pluginIndex = name.lastIndexOf('!');
+      if (pluginIndex != -1) {
+        var argumentName = name.substr(0, pluginIndex);
+
+        // plugin name is part after "!" or the extension itself
+        var pluginName = name.substr(pluginIndex + 1) || argumentName.substr(argumentName.lastIndexOf('.') + 1);
+
+        // normalize the plugin name relative to the same parent
+        return new Promise(function(resolve) {
+          resolve(loader.normalize(pluginName, parentName, parentAddress)); 
+        })
+        // normalize the plugin argument
+        .then(function(_pluginName) {
+          pluginName = _pluginName;
+          return loader.normalize(argumentName, parentName, parentAddress);
+        })
+        .then(function(argumentName) {
+          return argumentName + '!' + pluginName;
+        });
+      }
+
+      // standard normalization
+      return name;
+    });
+  }
+
+  var loaderLocate = loader.locate;
+  loader.locate = function(load) {
+    var loader = this;
+
+    var name = load.name;
+
+    // only fetch the plugin itself if this name isn't defined
+    if (this.defined && this.defined[name])
+      return loaderLocate.call(this, load);
+
+    // plugin
+    var pluginIndex = name.lastIndexOf('!');
+    if (pluginIndex != -1) {
+      var pluginName = name.substr(pluginIndex + 1);
+
+      // the name to locate is the plugin argument only
+      load.name = name.substr(0, pluginIndex);
+
+      var pluginLoader = loader.pluginLoader || loader;
+
+      // load the plugin module
+      // NB ideally should use pluginLoader.load for normalized,
+      //    but not currently working for some reason
+      return pluginLoader['import'](pluginName)
+      .then(function() {
+        var plugin = pluginLoader.get(pluginName);
+        plugin = plugin['default'] || plugin;
+
+        // allow plugins to opt-out of build
+        if (plugin.build === false && loader.pluginLoader)
+          load.metadata.build = false;
+
+        // store the plugin module itself on the metadata
+        load.metadata.plugin = plugin;
+        load.metadata.pluginName = pluginName;
+        load.metadata.pluginArgument = load.name;
+
+        // run plugin locate if given
+        if (plugin.locate)
+          return plugin.locate.call(loader, load);
+
+        // otherwise use standard locate without '.js' extension adding
+        else
+          return Promise.resolve(loader.locate(load))
+          .then(function(address) {
+            return address.substr(0, address.length - 3);
+          });
+      });
+    }
+
+    return loaderLocate.call(this, load);
+  }
+
+  var loaderFetch = loader.fetch;
+  loader.fetch = function(load) {
+    var loader = this;
+    if (load.metadata.build === false)
+      return '';
+    else if (load.metadata.plugin && load.metadata.plugin.fetch && !load.metadata.pluginFetchCalled) {
+      load.metadata.pluginFetchCalled = true;
+      return load.metadata.plugin.fetch.call(loader, load, loaderFetch);
+    }
+    else
+      return loaderFetch.call(loader, load);
+  }
+
+  var loaderTranslate = loader.translate;
+  loader.translate = function(load) {
+    var loader = this;
+    if (load.metadata.plugin && load.metadata.plugin.translate)
+      return Promise.resolve(load.metadata.plugin.translate.call(loader, load)).then(function(result) {
+        if (result)
+          load.source = result;
+        return loaderTranslate.call(loader, load);
+      });
+    else
+      return loaderTranslate.call(loader, load);
+  }
+
+  var loaderInstantiate = loader.instantiate;
+  loader.instantiate = function(load) {
+    var loader = this;
+    if (load.metadata.plugin && load.metadata.plugin.instantiate)
+      return Promise.resolve(load.metadata.plugin.instantiate.call(loader, load)).then(function(result) {
+        load.metadata.format = 'defined';
+        load.metadata.execute = function() {
+          return result;
+        };
+        return loaderInstantiate.call(loader, load);
+      });
+    else if (load.metadata.plugin && load.metadata.plugin.build === false) {
+      load.metadata.format = 'defined';
+      load.metadata.deps.push(load.metadata.pluginName);
+      load.metadata.execute = function() {
+        return loader.newModule({});
+      };
+      return loaderInstantiate.call(loader, load);
+    }
+    else
+      return loaderInstantiate.call(loader, load);
+  }
+
+}/*
   System bundles
 
   Allows a bundle module to be specified which will be dynamically 
@@ -1937,6 +2087,7 @@ global(System);
 cjs(System);
 amd(System);
 map(System);
+plugins(System);
 bundles(System);
 versions(System);
 depCache(System);
