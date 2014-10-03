62c2294011e0c1f0977de3ae82ce053d56f2ea3a
guybedford <guybedford@gmail.com>
2014-10-03 11:36:49 +0200

    support bundles defining into their own name

---
+++ test/tests/foo.js
@@ -0,0 +1,10 @@
+System.register("tests/foo", [], function($__export) {
+  "use strict";
+  var __moduleName = "foo";
+  return {
+    setters: [],
+    execute: function() {
+      $__export('f', 'f');
+    }
+  };
+});
