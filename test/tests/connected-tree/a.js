0aba1f046e5fde9801abb9499b51e03f4ae7fa04
Justin Meyer <justinbmeyer@gmail.com>
2015-01-10 20:40:23 -0600

    adds tests showing a problem with connected trees and es modules in the middle

---
+++ test/tests/connected-tree/a.js
@@ -0,0 +1,9 @@
+// a.js 
+var c = require("./c");
+var b = require('./b');
+
+module.exports = {
+	name: "a",
+	b: b,
+	c: c
+};
---
+++ test/tests/connected-tree/b.js
@@ -0,0 +1,6 @@
+import c from "./c";
+
+export default {
+	name: "b",
+	c: c
+};
