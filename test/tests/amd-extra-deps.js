1f60635c84ebcf6e372199c9fd00732fdc89cbd5
guybedford <guybedford@gmail.com>
2015-06-10 15:52:50 +0200

    multi deps shim fix (#494)

--- test/tests/amd-extra-deps.js
+++ test/tests/amd-extra-deps.js
@@ -1,5 +1,6 @@
-"deps[] ./amd-dep-A.js";
-"deps[] ./amd-dep-B.js";
+// NB update to "deps[]" when deps is deprecated
+"deps ./amd-dep-A.js";
+"deps ./amd-dep-B.js";
 
 define([], function() {
   return [A, B];
