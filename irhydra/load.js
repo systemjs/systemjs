d850b6a0d1263b0af12b3631779f537d0307117f
guybedford <guybedford@gmail.com>
2016-12-18 16:10:01 +0200

    add irhydra, play with resolving deoptimizations

---
+++ irhydra/load.js
@@ -0,0 +1,16 @@
+var System = require('../');
+
+function load () {
+  var loader = new System.constructor();
+  return loader.import('./test/tests/register-circular1.js');
+}
+
+var i = 100;
+
+function again () {
+  i--;
+  if (i)
+    return load().then(again);
+}
+
+again();
