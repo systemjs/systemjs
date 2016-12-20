93a70d435f8097f83548fb8a765d4e080ff17a43
guybedford <guybedford@gmail.com>
2016-12-20 14:15:36 +0200

    node production loader

---
+++ index-production.js
@@ -0,0 +1,132 @@
+var SystemProduction = require('./dist/system-production.src.js');
+var fs = require('fs');
+var path = require('path');
+var Module = require('module');
+
+var babel = require('systemjs-plugin-babel/systemjs-babel-node.js').babel;
+var modulesRegister = require('systemjs-plugin-babel/systemjs-babel-node.js').modulesRegister;
+
+var isWindows = process.platform.match(/^win/);
+function fileUrlToPath (fileUrl) {
+  if (fileUrl.substr(0, 7) !== 'file://')
+    throw new RangeError(fileUrl + ' is not a valid file url');
+  if (isWindows)
+    return fileUrl.substr(8).replace(/\//g, '/');
+  else
+    return fileUrl.substr(7);
+}
+
+var sourceMapSources = global.systemjsProductionNodeLoaderSourceMapSources = global.systemjsProductionNodeLoaderSourceMapSources || {};
+require('source-map-support').install({
+  retrieveSourceMap: function (source) {
+    if (!sourceMapSources[source])
+      return null;
+
+    return {
+      url: source.replace('!transpiled', ''),
+      map: sourceMapSources[source]
+    };
+  }
+});
+
+var SystemJSProductionLoader = SystemProduction.constructor;
+
+function SystemJSProductionNodeLoader () {
+  SystemJSProductionLoader.call(this);
+
+  // ensure System.register is available
+  global.System = global.System || {};
+  global.System.register = this.register.bind(this);
+}
+
+SystemJSProductionNodeLoader.prototype = Object.create(SystemJSProductionLoader.prototype);
+SystemJSProductionNodeLoader.prototype.constructor = SystemJSProductionNodeLoader;
+
+var plainResolveSync = SystemJSProductionLoader.prototype[SystemJSProductionLoader.plainResolveSync];
+
+// add node_modules loading to plain resolve
+SystemJSProductionNodeLoader.prototype[SystemJSProductionLoader.plainResolve] =
+SystemJSProductionNodeLoader.prototype[SystemJSProductionLoader.plainResolveSync] = function (key, parentKey) {
+  // SystemJS map takes preference
+  // SystemJS map targets entirely skip Node resolution process
+  var resolved = plainResolveSync.call(this, key, parentKey);
+  if (resolved !== undefined)
+    return resolved;
+
+  var parentPath = parentKey ? fileUrlToPath(parentKey) : process.cwd();
+  var requireContext = new Module(parentPath);
+  requireContext.paths = Module._nodeModulePaths(parentPath);
+
+  try {
+    resolved = Module._resolveFilename(key.substr(0, 5) === 'file:' ? fileUrlToPath(key) : key, requireContext, true);
+  }
+  catch (e) {
+    if (e.code === 'MODULE_NOT_FOUND')
+      return;
+    throw e;
+  }
+
+  // core modules are returned as plain non-absolute paths -> convert into node:fs etc to have a URL
+  return path.isAbsolute(resolved) ? pathToFileUrl(resolved) : 'node:' + resolved;
+};
+
+SystemJSProductionNodeLoader.prototype[SystemJSProductionLoader.instantiate] = function (key, processAnonRegister) {
+  var loader = this;
+
+  // first, try to load the module as CommonJS
+  var path = fileUrlToPath(key);
+  var nodeModule = tryNodeLoad(key.substr(0, 5) === 'node:' ? key.substr(5) : path);
+
+  if (nodeModule) {
+    // if it was System.register, then ignore the node loaded exports
+    if (processAnonRegister())
+      return;
+    // CommonJS
+    return Promise.resolve(loader.newModule({
+      default: nodeModule
+    }));
+  }
+
+  // otherwise, load as ES converting into System.register
+  return new Promise(function (resolve, reject) {
+    fs.readFile(path, function (err, source) {
+      if (err)
+        return reject(err);
+
+      // transform source with Babel
+      var output = babel.transform(source, {
+        compact: false,
+        filename: key + '!transpiled',
+        sourceFileName: key,
+        moduleIds: false,
+        sourceMaps: 'both',
+        presets: [modulesRegister]
+      });
+
+      // evaluate without require, exports and module variables
+      var path = path + '!transpiled';
+      output.map.sources = output.map.sources.map(fileUrlToPath);
+      sourceMapSources[path] = output.map;
+      (0, eval)(output.code + '\n//# sourceURL=' + path);
+      processAnonRegister();
+
+      resolve();
+    });
+  });
+};
+
+function tryNodeLoad (path) {
+  try {
+    return require(path);
+  }
+  catch(e) {
+    if (e instanceof SyntaxError &&
+        (e.message.indexOf('Unexpected token export') !== -1 || e.message.indexOf('Unexpected token import') !== -1 ||
+        // for Node 4 and less we verify the reserved word is exactly 6 chars for import / export
+        e.message.indexOf('Unexpected reserved word') !== -1) && e.message.indexOf(' ^^^^^^\n') !== -1)
+      return;
+    throw e;
+  }
+}
+
+module.exports = new SystemJSProductionNodeLoader();
--- test/test-production.js
+++ test/test-production.js
@@ -1,3 +1,19 @@
+if (typeof System === 'undefined') {
+  global.System = require('../index-production.js');
+  global.System.config({
+    baseURL: 'test'
+  });
+}
+
+var base;
+if (typeof document !== 'undefined') {
+  base = document.baseURI.substr(0, document.baseURI.lastIndexOf('/') + 1);
+}
+else {
+  var cwd = process.cwd();
+  base = 'file://' + (cwd[0] !== '/' ? '/' : '') + cwd + '/test/';
+}
+
 suite('SystemJS Standard Tests', function() {
 
   function ok(assertion, message) {
@@ -23,12 +39,62 @@ suite('SystemJS Standard Tests', function() {
     return System.import('tests/string-encoding.js').then(function (m) {
       ok(m.pi === decodeURI('%CF%80'));
       ok(m.emoji === decodeURI('%F0%9F%90%B6'));
-    })
-  })
+    });
+  });
 
   test('Support the empty module', function () {
     return System.import('@empty').then(function (m) {
       ok(m, 'No empty module');
+    });
+  });
+
+  test('Loading an empty module', function () {
+    return System.import('tests/empty.js').then(function (m) {
+      if (typeof process !== 'undefined')
+        ok(m);
+      else
+        throw new Error('Empty module should not supported in browsers.');
+    }, function (err) {
+      if (typeof process === 'undefined')
+        ok(err);
+      else
+        throw new Error('Empty module should be supported in Node.');
+    })
+  });
+
+  test('Paths configuration', function () {
+    System.config({
+      map: {
+        'f/': 'a:',
+        'g': 'https://site.com'
+      },
+      paths: {
+        'a': 'b',
+        'a:': 'c/',
+        'b/': 'd/',
+        'b.js': 'http://jquery.com/jquery.js',
+        'https://site.com/': 'https://another.com/'
+      }
+    });
+
+    return Promise.all([
+      System.resolve('a'),
+      System.resolve('f/b'),
+      System.resolve('a/b'),
+      System.resolve('b/c'),
+      System.resolve('b'),
+      System.resolve('b.js'),
+      System.resolve('b.js/c'),
+      System.resolve('g/x')
+    ]).then(function (a) {
+      ok(a[0] === base + 'b');
+      ok(a[1] === base + 'c/b');
+      ok(a[2] === base + 'b/b');
+      ok(a[3] === base + 'd/c');
+      ok(a[4] === base + 'b');
+      ok(a[5] === 'http://jquery.com/jquery.js');
+      ok(a[6] === 'http://jquery.com/jquery.js/c');
+      ok(a[7] === 'https://another.com/x');
     });
   });
 
@@ -91,7 +157,7 @@ suite('SystemJS Standard Tests', function() {
 
   test('System.register module name arg', function () {
     return System.import('tests/module-name.js').then(function (m) {
-      ok(m.name == new URL('tests/module-name.js', document.baseURI));
+      ok(m.name == base + 'tests/module-name.js');
     });
   });
 
@@ -101,8 +167,26 @@ suite('SystemJS Standard Tests', function() {
         'tests/group-test.js': ['group-a']
       }
     });
-    return System.import('group-a').then(function (m) {
-      ok(m);
+    return Promise.resolve()
+    .then(function () {
+      if (typeof process !== 'undefined')
+        return System.import('tests/group-test.js');
+    })
+    .then(function () {
+      return System.import('group-a').then(function (m) {
+        ok(m);
+      });
+    });
+  });
+
+  test('Loading bundle not containing the module', function () {
+    System.config({
+      bundles: {
+        'tests/group-test.js': ['not-in-bundle']
+      }
+    });
+    return System.import('tests/group-test.js').catch(function (e) {
+      ok(e.toString().indexOf('Error: Module instantiation did not call an anonymous or correctly named System.register.') === 0);
     });
   });
 
@@ -112,8 +196,15 @@ suite('SystemJS Standard Tests', function() {
         'tests/mixed-bundle.js': ['tree/third', 'tree/cjs', 'tree/jquery', 'tree/second', 'tree/global', 'tree/amd', 'tree/first']
       }
     });
-    return System.import('tree/third').then(function (m) {
-      ok(m.some == 'exports');
+    return Promise.resolve()
+    .then(function () {
+      if (typeof process !== 'undefined')
+        return System.import('tests/mixed-bundle.js');
+    })
+    .then(function () {
+      return System.import('tree/third').then(function (m) {
+        ok(m.some == 'exports');
+      });
     });
   });
 
