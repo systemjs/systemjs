8afdc93275a376b99cfdd321aaffc94a7ed0b703
guybedford <guybedford@gmail.com>
2017-01-24 14:00:03 +0200

    upgrade es-module-loader, angular test, ensure System is written still

--- src/system.js
+++ src/system.js
@@ -6,23 +6,8 @@ SystemJSLoader.prototype.version = VERSION;
 var System = new SystemJSLoader();
 
 // only set the global System on the global in browsers
-if (isBrowser || isWorker) {
-  global.SystemJS = System;
-
-  // dont override an existing System global
-  if (!global.System) {
-    global.System = System;
-  }
-  // rather just extend or set a System.register on the existing System global
-  else {
-    var register = global.System.register;
-    global.System.register = function () {
-      if (register)
-        register.apply(this, arguments);
-      System.register.apply(this, arguments);
-    };
-  }
-}
+if (isBrowser || isWorker)
+  global.SystemJS = global.System = System;
 
 if (typeof module !== 'undefined' && module.exports)
   module.exports = System;
---
+++ test/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent { name = 'Angular'; }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
