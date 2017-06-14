bd61a6a6f40b1114ff68cb157be6dbc6d2574123
guybedford <guybedford@gmail.com>
2017-06-14 13:44:05 +0200

    0.20.14

--- dist/system-production.src.js
+++ dist/system-production.src.js
@@ -1,5 +1,5 @@
 /*
- * SystemJS v0.20.13 Production
+ * SystemJS v0.20.14 Production
  */
 (function () {
 'use strict';
@@ -99,11 +99,9 @@ function LoaderError__Check_error_message_for_loader_stack (childErr, newMessage
 function throwResolveError (relUrl, parentUrl) {
   throw new RangeError('Unable to resolve "' + relUrl + '" to ' + parentUrl);
 }
-var protocolRegEx = /^[^/]+:/;
 function resolveIfNotPlain (relUrl, parentUrl) {
   relUrl = relUrl.trim();
-  if (parentUrl)
-    var parentProtocol = parentUrl.match(protocolRegEx);
+  var parentProtocol = parentUrl && parentUrl.substr(0, parentUrl.indexOf(':') + 1);
 
   var firstChar = relUrl[0];
   var secondChar = relUrl[1];
@@ -112,12 +110,13 @@ function resolveIfNotPlain (relUrl, parentUrl) {
   if (firstChar === '/' && secondChar === '/') {
     if (!parentProtocol)
       throwResolveError(relUrl, parentUrl);
-    return parentProtocol[0] + relUrl;
+    return parentProtocol + relUrl;
   }
   // relative-url
-  else if (firstChar === '.' && (secondChar === '/' || secondChar === '.' && (relUrl[2] === '/' || relUrl.length === 2) || relUrl.length === 1)
-      || firstChar === '/') {
-    var parentIsPlain = !parentProtocol || parentUrl[parentProtocol[0].length] !== '/';
+  else if (firstChar === '.' && (secondChar === '/' || secondChar === '.' && (relUrl[2] === '/' || relUrl.length === 2 && (relUrl += '/')) ||
+      relUrl.length === 1  && (relUrl += '/')) ||
+      firstChar === '/') {
+    var parentIsPlain = !parentProtocol || parentUrl[parentProtocol.length] !== '/';
 
     // read pathname from parent if a URL
     // pathname taken to be part after leading "/"
@@ -128,10 +127,10 @@ function resolveIfNotPlain (relUrl, parentUrl) {
         throwResolveError(relUrl, parentUrl);
       pathname = parentUrl;
     }
-    else if (parentUrl[parentProtocol[0].length + 1] === '/') {
+    else if (parentUrl[parentProtocol.length + 1] === '/') {
       // resolving to a :// so we need to read out the auth and host
-      if (parentProtocol[0] !== 'file:') {
-        pathname = parentUrl.substr(parentProtocol[0].length + 2);
+      if (parentProtocol !== 'file:') {
+        pathname = parentUrl.substr(parentProtocol.length + 2);
         pathname = pathname.substr(pathname.indexOf('/') + 1);
       }
       else {
@@ -140,7 +139,7 @@ function resolveIfNotPlain (relUrl, parentUrl) {
     }
     else {
       // resolving to :/ so pathname is the /... part
-      pathname = parentUrl.substr(parentProtocol[0].length + 1);
+      pathname = parentUrl.substr(parentProtocol.length + 1);
     }
 
     if (firstChar === '/') {
@@ -156,14 +155,14 @@ function resolveIfNotPlain (relUrl, parentUrl) {
     var segmented = pathname.substr(0, pathname.lastIndexOf('/') + 1) + relUrl;
 
     var output = [];
-    var segmentIndex = undefined;
+    var segmentIndex = -1;
 
     for (var i = 0; i < segmented.length; i++) {
       // busy reading a segment - only terminate on '/'
-      if (segmentIndex !== undefined) {
+      if (segmentIndex !== -1) {
         if (segmented[i] === '/') {
-          output.push(segmented.substr(segmentIndex, i - segmentIndex + 1));
-          segmentIndex = undefined;
+          output.push(segmented.substring(segmentIndex, i + 1));
+          segmentIndex = -1;
         }
         continue;
       }
@@ -171,12 +170,12 @@ function resolveIfNotPlain (relUrl, parentUrl) {
       // new segment - check if it is relative
       if (segmented[i] === '.') {
         // ../ segment
-        if (segmented[i + 1] === '.' && (segmented[i + 2] === '/' || i === segmented.length - 2)) {
+        if (segmented[i + 1] === '.' && segmented[i + 2] === '/') {
           output.pop();
           i += 2;
         }
         // ./ segment
-        else if (segmented[i + 1] === '/' || i === segmented.length - 1) {
+        else if (segmented[i + 1] === '/') {
           i += 1;
         }
         else {
@@ -199,8 +198,8 @@ function resolveIfNotPlain (relUrl, parentUrl) {
       segmentIndex = i;
     }
     // finish reading out the last segment
-    if (segmentIndex !== undefined)
-      output.push(segmented.substr(segmentIndex, segmented.length - segmentIndex));
+    if (segmentIndex !== -1)
+      output.push(segmented.substr(segmentIndex));
 
     return parentUrl.substr(0, parentUrl.length - pathname.length) + output.join('');
   }
@@ -1465,7 +1464,7 @@ systemJSPrototype.resolveSync = function (key, parentKey) {
 systemJSPrototype.import = function () {
   return RegisterLoader$1.prototype.import.apply(this, arguments)
   .then(function (m) {
-    return m.__useDefault || m;
+    return '__useDefault' in m ? m.__useDefault : m;
   });
 };
 
@@ -1685,7 +1684,7 @@ function coreInstantiate (key, processAnonRegister) {
   return doScriptLoad(key, processAnonRegister);
 }
 
-SystemJSProductionLoader$1.prototype.version = "0.20.13 Production";
+SystemJSProductionLoader$1.prototype.version = "0.20.14 Production";
 
 var System = new SystemJSProductionLoader$1();
 
--- dist/system.src.js
+++ dist/system.src.js
@@ -1,5 +1,5 @@
 /*
- * SystemJS v0.20.13 Dev
+ * SystemJS v0.20.14 Dev
  */
 (function () {
 'use strict';
@@ -99,11 +99,9 @@ function LoaderError__Check_error_message_for_loader_stack (childErr, newMessage
 function throwResolveError (relUrl, parentUrl) {
   throw new RangeError('Unable to resolve "' + relUrl + '" to ' + parentUrl);
 }
-var protocolRegEx = /^[^/]+:/;
 function resolveIfNotPlain (relUrl, parentUrl) {
   relUrl = relUrl.trim();
-  if (parentUrl)
-    var parentProtocol = parentUrl.match(protocolRegEx);
+  var parentProtocol = parentUrl && parentUrl.substr(0, parentUrl.indexOf(':') + 1);
 
   var firstChar = relUrl[0];
   var secondChar = relUrl[1];
@@ -112,12 +110,13 @@ function resolveIfNotPlain (relUrl, parentUrl) {
   if (firstChar === '/' && secondChar === '/') {
     if (!parentProtocol)
       throwResolveError(relUrl, parentUrl);
-    return parentProtocol[0] + relUrl;
+    return parentProtocol + relUrl;
   }
   // relative-url
-  else if (firstChar === '.' && (secondChar === '/' || secondChar === '.' && (relUrl[2] === '/' || relUrl.length === 2) || relUrl.length === 1)
-      || firstChar === '/') {
-    var parentIsPlain = !parentProtocol || parentUrl[parentProtocol[0].length] !== '/';
+  else if (firstChar === '.' && (secondChar === '/' || secondChar === '.' && (relUrl[2] === '/' || relUrl.length === 2 && (relUrl += '/')) ||
+      relUrl.length === 1  && (relUrl += '/')) ||
+      firstChar === '/') {
+    var parentIsPlain = !parentProtocol || parentUrl[parentProtocol.length] !== '/';
 
     // read pathname from parent if a URL
     // pathname taken to be part after leading "/"
@@ -128,10 +127,10 @@ function resolveIfNotPlain (relUrl, parentUrl) {
         throwResolveError(relUrl, parentUrl);
       pathname = parentUrl;
     }
-    else if (parentUrl[parentProtocol[0].length + 1] === '/') {
+    else if (parentUrl[parentProtocol.length + 1] === '/') {
       // resolving to a :// so we need to read out the auth and host
-      if (parentProtocol[0] !== 'file:') {
-        pathname = parentUrl.substr(parentProtocol[0].length + 2);
+      if (parentProtocol !== 'file:') {
+        pathname = parentUrl.substr(parentProtocol.length + 2);
         pathname = pathname.substr(pathname.indexOf('/') + 1);
       }
       else {
@@ -140,7 +139,7 @@ function resolveIfNotPlain (relUrl, parentUrl) {
     }
     else {
       // resolving to :/ so pathname is the /... part
-      pathname = parentUrl.substr(parentProtocol[0].length + 1);
+      pathname = parentUrl.substr(parentProtocol.length + 1);
     }
 
     if (firstChar === '/') {
@@ -156,14 +155,14 @@ function resolveIfNotPlain (relUrl, parentUrl) {
     var segmented = pathname.substr(0, pathname.lastIndexOf('/') + 1) + relUrl;
 
     var output = [];
-    var segmentIndex = undefined;
+    var segmentIndex = -1;
 
     for (var i = 0; i < segmented.length; i++) {
       // busy reading a segment - only terminate on '/'
-      if (segmentIndex !== undefined) {
+      if (segmentIndex !== -1) {
         if (segmented[i] === '/') {
-          output.push(segmented.substr(segmentIndex, i - segmentIndex + 1));
-          segmentIndex = undefined;
+          output.push(segmented.substring(segmentIndex, i + 1));
+          segmentIndex = -1;
         }
         continue;
       }
@@ -171,12 +170,12 @@ function resolveIfNotPlain (relUrl, parentUrl) {
       // new segment - check if it is relative
       if (segmented[i] === '.') {
         // ../ segment
-        if (segmented[i + 1] === '.' && (segmented[i + 2] === '/' || i === segmented.length - 2)) {
+        if (segmented[i + 1] === '.' && segmented[i + 2] === '/') {
           output.pop();
           i += 2;
         }
         // ./ segment
-        else if (segmented[i + 1] === '/' || i === segmented.length - 1) {
+        else if (segmented[i + 1] === '/') {
           i += 1;
         }
         else {
@@ -199,8 +198,8 @@ function resolveIfNotPlain (relUrl, parentUrl) {
       segmentIndex = i;
     }
     // finish reading out the last segment
-    if (segmentIndex !== undefined)
-      output.push(segmented.substr(segmentIndex, segmented.length - segmentIndex));
+    if (segmentIndex !== -1)
+      output.push(segmented.substr(segmentIndex));
 
     return parentUrl.substr(0, parentUrl.length - pathname.length) + output.join('');
   }
@@ -2208,7 +2207,7 @@ function doMap (loader, config, pkg, pkgKey, mapMatch, path, metadata, skipExten
     // first map condition to match is used
     for (var i = 0; i < conditions.length; i++) {
       var c = conditions[i].condition;
-      var value = readMemberExpression(c.prop, conditionValues[i].__useDefault || conditionValues[i]);
+      var value = readMemberExpression(c.prop, '__useDefault' in conditionValues[i] ? conditionValues[i].__useDefault : conditionValues[i]);
       if (!c.negate && value || c.negate && !value)
         return conditions[i].map;
     }
@@ -2855,8 +2854,6 @@ var formatHelpers = function (loader) {
       for (var i = 0; i < names.length; i++)
         dynamicRequires.push(loader.import(names[i], referer));
       Promise.all(dynamicRequires).then(function (modules) {
-        for (var i = 0; i < modules.length; i++)
-          modules[i] = modules[i].__useDefault || modules[i];
         if (callback)
           callback.apply(null, modules);
       }, errback);
@@ -2868,7 +2865,7 @@ var formatHelpers = function (loader) {
       var module = loader.get(normalized);
       if (!module)
         throw new Error('Module not already loaded loading "' + names + '" as ' + normalized + (referer ? ' from "' + referer + '".' : '.'));
-      return module.__useDefault || module;
+      return '__useDefault' in module ? module.__useDefault : module;
     }
 
     else
@@ -3918,7 +3915,7 @@ SystemJSLoader$1.prototype.global = envGlobal;
 SystemJSLoader$1.prototype.import = function () {
   return RegisterLoader$1.prototype.import.apply(this, arguments)
   .then(function (m) {
-    return m.__useDefault || m;
+    return '__useDefault' in m ? m.__useDefault : m;
   });
 };
 
@@ -3987,7 +3984,7 @@ SystemJSLoader$1.prototype.registerDynamic = function (key, deps, executingRequi
   return RegisterLoader$1.prototype.registerDynamic.call(this, key, deps, executingRequire, execute);
 };
 
-SystemJSLoader$1.prototype.version = "0.20.13 Dev";
+SystemJSLoader$1.prototype.version = "0.20.14 Dev";
 
 var System = new SystemJSLoader$1();
 
