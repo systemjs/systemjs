45f9de39e42e82f0a2ccfd9748f0a79123e10029
Guy Bedford <guybedford@gmail.com>
2018-08-25 13:41:28 +0200

    get to return null for errored

--- src/features/registry.js
+++ src/features/registry.js
@@ -2,18 +2,21 @@ import { systemJSPrototype, REGISTRY } from '../system-core.js';
 
 systemJSPrototype.get = function (id) {
   const load = this[REGISTRY][id];
-  if (load && load.e === null && !load.E)
+  if (load && load.e === null && !load.E) {
+    if (load.eE)
+      return null;
     return load.n;
+  }
 };
 
 // Delete function provided for hot-reloading use cases
 systemJSPrototype.delete = function (id) {
   const load = this.get(id);
-  if (!load)
+  if (load === undefined)
     return false;
   // remove from importerSetters
   // (release for gc)
-  if (load.d)
+  if (load && load.d)
     load.d.forEach(function (depLoad) {
       const importerIndex = depLoad.i.indexOf(load);
       if (importerIndex !== -1)
