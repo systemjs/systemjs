772c8ffaa18523a52fa4c0ca0b95d0be34d8b25d
Guy Bedford <guybedford@gmail.com>
2018-10-06 21:12:11 +0200

    correct resolution adjustment

--- src/common.js
+++ src/common.js
@@ -42,7 +42,7 @@ export function resolveIfNotPlainOrUrl (relUrl, parentUrl) {
     }
     else {
       // resolving to :/ so pathname is the /... part
-      pathname = parentUrl.slice(parentProtocol.length + (parentProtocol[parentProtocol.length] === '/' ? 1 : 0));
+      pathname = parentUrl.slice(parentProtocol.length + (parentUrl[parentProtocol.length] === '/'));
     }
 
     if (relUrl[0] === '/')
