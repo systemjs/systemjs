0d12c9dd565cc1d628cb7cf592f7449b39a6d098
Joel Denning <joeldenning@gmail.com>
2019-03-12 12:48:16 -0600

    Eagerly downloading import maps and adding tests.

--- src/features/import-map.js
+++ src/features/import-map.js
@@ -4,28 +4,42 @@
  * <script type="systemjs-importmap">{}</script>
  * OR
  * <script type="systemjs-importmap" src=package.json></script>
- * 
- * Only supports loading the first import map
  */
 import { baseUrl as pageBaseUrl, parseImportMap, resolveImportMap } from '../common.js';
 import { systemJSPrototype } from '../system-core.js';
 
-var acquiringImportMaps = true, importMapPromise;
+var importMapPromise, fetchPromises = {};
+
+// Start downloading the import maps as soon as possible
+if (typeof document !== 'undefined') {
+  const scriptsToDownload = document.querySelectorAll('script[type="systemjs-importmap"][src]');
+
+  for (let i = 0; i < scriptsToDownload.length; i++) {
+    const script = scriptsToDownload[i]
+    fetchPromises[script.src] = fetch(script.src)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        return parseImportMap(data, script.src);
+      })
+  }
+}
 
 function retrieveImportMaps() {
   if (typeof document !== 'undefined') {
-    const scripts = document.getElementsByTagName('script');
+    // We must apply/merge the import maps in the order that they appear in the dom
+    const scripts = document.querySelectorAll('script[type="systemjs-importmap"]');
     let pendingMaps = []
 
     for (let i = 0; i < scripts.length; i++) {
       const script = scripts[i];
-      if (script.type !== 'systemjs-importmap')
-        continue;
-
       if (!script.src) {
         pendingMaps.push(parseImportMap(JSON.parse(script.innerHTML), pageBaseUrl));
       }
-      else {
+      else if (fetchPromises[script.src]) {
+        pendingMaps.push(fetchPromises[script.src])
+      } else {
         pendingMaps.push(
           fetch(script.src)
           .then(function (res) {
@@ -38,9 +52,12 @@ function retrieveImportMaps() {
       }
     }
 
+    // free the memory for this object -- we don't need it anymore after the first System.import happens
+    fetchPromises = null
+
     const initialImportMap = { imports: {}, scopes: {} };
 
-    importMapPromise = Promise.all(pendingMaps).then(function(importMaps) {
+    return Promise.all(pendingMaps).then(function(importMaps) {
       return importMaps.reduce(function(finalMap, importMap) {
         return mergeImportMap(finalMap, importMap);
       }, initialImportMap);
@@ -48,7 +65,7 @@ function retrieveImportMaps() {
   }
 }
 
-function mergeImportMap(originalMap, newMap) {
+export function mergeImportMap(originalMap, newMap) {
   for (let i in newMap.imports) {
     originalMap.imports[i] = newMap.imports[i];
   }
@@ -65,9 +82,8 @@ function mergeImportMap(originalMap, newMap) {
 systemJSPrototype.resolve = function (id, parentUrl) {
   parentUrl = parentUrl || pageBaseUrl;
 
-  if (acquiringImportMaps) {
-    acquiringImportMaps = false;
-    retrieveImportMaps();
+  if (!importMapPromise) {
+    importMapPromise = retrieveImportMaps();
   }
 
   return importMapPromise
