aadc2a4742872e6d6f1f0a6e11c3507037709b88
Joel Denning <joeldenning@gmail.com>
2019-06-07 10:04:39 -0600

    Implementing loading JSON modules. Resolves #1949.

--- src/features/script-load.js
+++ src/features/script-load.js
@@ -18,26 +18,36 @@ systemJSPrototype.register = function (deps, declare) {
 
 systemJSPrototype.instantiate = function (url, firstParentUrl) {
   const loader = this;
-  return new Promise(function (resolve, reject) {
-    const script = document.createElement('script');
-    script.charset = 'utf-8';
-    script.async = true;
-    script.crossOrigin = 'anonymous';
-    script.addEventListener('error', function () {
-      reject(new Error('Error loading ' + url + (firstParentUrl ? ' from ' + firstParentUrl : '')));
+  if (url.endsWith('.json')) {
+    return fetch(url).then(function (resp) {
+      return resp.json()
+    }).then(function (json) {
+      return [[], function(_export) {
+        return {execute: function() {_export('default', json)}}
+      }];
+    })
+  } else {
+    return new Promise(function (resolve, reject) {
+      const script = document.createElement('script');
+      script.charset = 'utf-8';
+      script.async = true;
+      script.crossOrigin = 'anonymous';
+      script.addEventListener('error', function () {
+        reject(new Error('Error loading ' + url + (firstParentUrl ? ' from ' + firstParentUrl : '')));
+      });
+      script.addEventListener('load', function () {
+        document.head.removeChild(script);
+        // Note URL normalization issues are going to be a careful concern here
+        if (err) {
+          reject(err);
+          return err = undefined;
+        }
+        else {
+          resolve(loader.getRegister());
+        }
+      });
+      script.src = url;
+      document.head.appendChild(script);
     });
-    script.addEventListener('load', function () {
-      document.head.removeChild(script);
-      // Note URL normalization issues are going to be a careful concern here
-      if (err) {
-        reject(err);
-        return err = undefined;
-      }
-      else {
-        resolve(loader.getRegister());
-      }
-    });
-    script.src = url;
-    document.head.appendChild(script);
-  });
+  }
 };
