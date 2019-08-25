5632beb3c7e1daafa77d0e9af1f5c5538787c8ed
Joel Denning <joeldenning@gmail.com>
2019-08-25 11:21:13 -0600

    Reserve html modules for future implementation. See #1992.

--- src/features/script-load.js
+++ src/features/script-load.js
@@ -22,6 +22,8 @@ systemJSPrototype.instantiate = function (url, firstParentUrl) {
       stylesheet.replaceSync(source);
       _export('default', stylesheet);
     });
+  } else if (url.substr(-5) === '.html') {
+    return Promise.reject(Error("HTML modules are not yet implemented - " + url));
   } else {
     return new Promise(function (resolve, reject) {
       let err;
