0af3e36d674dbe875c712d75940cfdfae66ea086
Joel Denning <joeldenning@gmail.com>
2019-09-11 20:23:47 -0600

    Patching prepareImport before loading script modules. Resolves #2024.

--- src/system.js
+++ src/system.js
@@ -1,6 +1,6 @@
+import './features/import-map.js';
 import './features/script-load.js';
 import './features/worker-load.js';
 import './extras/global.js';
 import './extras/module-types.js';
-import './features/import-map.js';
 import './features/registry.js';
---
+++ test/fixtures/browser/systemjs-module-early.js
@@ -0,0 +1,7 @@
+System.register([], function(_export) {
+  return {
+    execute: function () {
+      _export('hi', 'bye');
+    }
+  };
+});
