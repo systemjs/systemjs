fbb9c073c121e429b1843fe7b08e7fd8159a8916
Joel Denning <joeldenning@gmail.com>
2020-01-16 14:31:34 -0700

    Fixing bug - named AMD modules were instantiated twice. Resolves #2103.

--- test/browser/named-register.js
+++ test/browser/named-register.js
@@ -11,7 +11,7 @@ suite('Named System.register', function() {
 
   test('Loading a named AMD bundle', function () {
     return System.import('./fixtures/browser/named-amd.js').then(function (m) {
-      assert.equal(Object.keys(m).length, 2);
+      assert.equal(m.a, 'b');
       return System.import('c');
     })
     .then(function (m) {
@@ -49,6 +49,19 @@ suite('Named System.register', function() {
       assert.ok(m.default);
       assert.ok(m.default.foo);
       assert.ok(m.foo);
+    });
+  });
+
+  test('loading named define module should work and should not instantiate it twice', function () {
+    return System.import('fixtures/amd-named-single-execute.js').then(function (m) {
+      console.log('m', m)
+      assert.equal(m.default, 'The first named AMD module');
+      assert.equal(numNamedAMDExecutions, 1);
+    }).then(function () {
+      return System.import('fixtures/amd-named-single-execute.js').then(function (m) {
+        assert.equal(m.default, 'The first named AMD module');
+        assert.equal(numNamedAMDExecutions, 1);
+      });
     });
   });
 });
