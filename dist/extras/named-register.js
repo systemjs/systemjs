085f150192a6536eae0659589793f728b0f3a1be
K J Kim <kj.kim@salesforce.com>
2020-02-23 14:22:18 -0800

    chore: reverting dist

--- dist/extras/named-register.js
+++ dist/extras/named-register.js
@@ -44,22 +44,15 @@
       // Prefer import map (or other existing) resolution over the registerRegistry
       return resolve.call(this, id, parentURL);
     } catch (err) {
-      if (id in this.registerRegistry) {
+      if (id in this.registerRegistry)
         return id;
-      }
       throw err;
     }
   };
 
   const instantiate = systemJSPrototype.instantiate;
   systemJSPrototype.instantiate = function (url, firstParentUrl) {
-    const result = this.registerRegistry[url];
-    if (result) {
-      this.registerRegistry[url] = null;
-      return result;
-    } else {
-      return instantiate.call(this, url, firstParentUrl);
-    }
+    return this.registerRegistry[url] || instantiate.call(this, url, firstParentUrl);
   };
 
   const getRegister = systemJSPrototype.getRegister;
