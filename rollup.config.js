0c1438d319c267688f2878030fa94777def262fd
Joel Denning <joeldenning@gmail.com>
2020-03-28 19:37:22 -0600

    Build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,101 @@
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import fs from 'fs';
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
 
 const version = JSON.parse(fs.readFileSync('package.json')).version;
-const name = process.env.sjs ? 's' : 'system';
-
-export default {
-  input: `src/${name}.js`,
-  output: {
-    format: 'iife',
-    strict: false,
-    file: `dist/${name}.js`,
-    banner: process.env.sjs ? `/*
-* SJS ${version}
-* Minimal SystemJS Build
-*/` : `/*
-* SystemJS ${version}
-*/`
+const extras = fs.readdirSync(path.resolve(__dirname, 'src/extras'))
+const terserOptions = {
+  mangle: {
+    eval: true,
+    module: true,
+    safari10: true,
   },
-  plugins: [replace({
-    TRACING: process.env.sjs ? 'false' : 'true'
-  })]
+  parse: {
+  },
+  compress: {
+    unsafe: true,
+    arguments: true,
+    booleans_as_integers: true,
+    hoist_funs: true,
+    keep_fargs: false,
+    module: true,
+    passes: 2,
+    toplevel: true
+  },
+  output: {
+    comments(node, comment) {
+      return /^\* SystemJS [0-9]\./.test(comment.value.trim());
+    },
+    safari10: true,
+  },
+  ecma: 5, // specify one of: 5, 2015, 2016, 2017 or 2018
+  keep_classnames: false,
+  keep_fnames: false,
+  ie8: false,
+  module: true,
+  nameCache: null, // or specify a name cache object
+  safari10: true,
+  toplevel: true,
+  warnings: false,
 };
+
+export default [
+  // mainConfig('system', true),
+  mainConfig('system', false),
+  // mainConfig('s', true),
+  // mainConfig('s', false),
+  // ...extrasConfig(true),
+  // ...extrasConfig(false),
+]
+
+function mainConfig(name, isDev) {
+  const sjs = name === 's'
+  let banner
+  if (sjs) {
+    banner = isDev ? `/*
+  * SJS ${version}
+  * Minimal SystemJS Build
+  */` : null;
+  } else {
+    banner = `/*
+  * SystemJS ${version}
+  */`
+  }
+
+  return {
+    input: `src/${name}.js`,
+    output: {
+      file: `dist/${name}${isDev ? '' : '.min'}.js`,
+      format: 'iife',
+      strict: false,
+      sourcemap: !isDev,
+      banner
+    },
+    plugins: [
+      replace({
+        TRACING: sjs ? 'false' : 'true',
+      }),
+      !isDev && terser(terserOptions)
+    ]
+  }
+}
+
+function extrasConfig(isDev) {
+  return extras.map(extra => {
+    extra = extra.replace('.js', '');
+    return {
+      input: `src/extras/${extra}.js`,
+      output: {
+        file: `dist/extras/${extra}${isDev ? '' : '.min'}.js`,
+        format: 'iife',
+        strict: false,
+        compact: true,
+        sourcemap: !isDev,
+      },
+      plugins: [
+        !isDev && terser(terserOptions)
+      ]
+    }
+  })
+}
