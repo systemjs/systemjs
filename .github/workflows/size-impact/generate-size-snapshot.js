3a1992ecb0572583d022c2d403e5a7b6a3ab2772
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-09 18:00:34 +0200

    Upgrade pull request size impact to 3.0.
    - Enable gzip and brotli tracking.
    - Split extras/** files tracking.

--- .github/workflows/size-impact/generate-size-snapshot.js
+++ .github/workflows/size-impact/generate-size-snapshot.js
@@ -1,12 +1,22 @@
-import { generateSnapshotFile } from "@jsenv/github-pull-request-filesize-impact";
+import {
+  generateSnapshotFile,
+  none,
+  gzip,
+  brotli,
+} from "@jsenv/github-pull-request-filesize-impact";
 
 generateSnapshotFile({
   projectDirectoryUrl: new URL("../../../", import.meta.url),
   snapshotFileRelativeUrl: process.argv[2],
-  directorySizeTrackingConfig: {
-    dist: {
-      "**/*": true,
-      "**/*.map": false,
+  trackingConfig: {
+    core: {
+      "./dist/*": true,
+      "./dist/*.map": false,
+    },
+    extras: {
+      "./dist/extras/**/*.js": true,
+      "./dist/extras/**/*.map": false,
     },
   },
+  transformations: { none, gzip, brotli },
 });
