1c415865ccdcdb6b597f0c8383cbc0f6389ad3d2
Hiroki Osame <osame@squareup.com>
2020-05-08 12:26:25 -0400

    Converted to ES5

--- src/extras/esm.js
+++ src/extras/esm.js
@@ -2,26 +2,30 @@
  * Interop for ESM modules to return the module export
  */
 (function (global) {
-  const esmPtrn = /\besm?\b/;
-  const getPath = url => {
-    const parsed = new URL(url, 'http://domain');
+  var esmPtrn = /\besm?\b/;
+  function getPath(url) {
+    var parsed = new URL(url, 'http://domain');
     return parsed.pathname + parsed.search + parsed.hash;
   };
-  const systemJSPrototype = global.System.constructor.prototype;
-  const originalInstantiate = systemJSPrototype.instantiate;
+  var systemJSPrototype = global.System.constructor.prototype;
+  var originalInstantiate = systemJSPrototype.instantiate;
   systemJSPrototype.instantiate = function (url) {
     if (!esmPtrn.test(getPath(url))) {
       return originalInstantiate.apply(this, arguments);
     }
 
-    return import(url).then((exported) => [
-      [],
-      (_export) => ({
-        execute() {
-          _export({ __useDefault: !!exported.default, ...exported });
-        }        
-      }),
-    ]);
+    return import(url).then(function (exported) {
+      return [
+        [],
+        function (_export) {
+          return {
+            execute() {
+              _export({ __useDefault: !!exported.default, ...exported });
+            }
+          };
+        },
+      ];
+    });
   };
 
 })(typeof self !== 'undefined' ? self : global);
