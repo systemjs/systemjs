90f3aa8f2f0a307ff9f16084eebf5c56ead004b8
Hiroki Osame <osame@squareup.com>
2020-05-26 00:56:23 -0400

    Removed debugging

--- test/fixtures/browser/es-modules/even.js
+++ test/fixtures/browser/es-modules/even.js
@@ -2,9 +2,6 @@ import { odd } from './odd.js'
 
 export var counter = 0;
 
-setInterval(() => {
-	console.log(counter);
-}, 1000)
 export function even(n) {
   counter++;
   return n == 0 || odd(n - 1);
