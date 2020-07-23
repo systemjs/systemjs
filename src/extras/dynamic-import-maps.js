26c71b23ffb925a5a88dc83310241496ef8dbe72
Guy Bedford <guybedford@gmail.com>
2020-07-23 13:12:40 -0700

    further simplification

--- src/extras/dynamic-import-maps.js
+++ src/extras/dynamic-import-maps.js
@@ -1,30 +1,16 @@
 /*
  * Support for live DOM updating import maps
  */
-(function () {
-  function handleMutation (mutations) {
-    var reparse = false;
-
-    for (var i = 0; i < mutations.length; i++) {
-      var mutation = mutations[i];
-      if (mutation.type !== 'childList')
-        continue;
-      for (var j = 0; j < mutation.addedNodes.length; j++) {
-        var addedNode = mutation.addedNodes[j];
-        if (addedNode.tagName === 'SCRIPT' && addedNode.type === 'systemjs-importmap' && !addedNode.sp) {
-          reparse = true;
-          break;
-        }
+new MutationObserver(function (mutations) {
+  for (var i = 0; i < mutations.length; i++) {
+    var mutation = mutations[i];
+    if (mutation.type === 'childList')
+    for (var j = 0; j < mutation.addedNodes.length; j++) {
+      var addedNode = mutation.addedNodes[j];
+      if (addedNode.tagName === 'SCRIPT' && addedNode.type === 'systemjs-importmap' && !addedNode.sp) {
+        System.prepareImport(true);
+        break;
       }
     }
-
-    if (reparse)
-      System.prepareImport(true);
   }
-
-  new MutationObserver(handleMutation).observe(document.documentElement, {
-    attributes: false,
-    childList: true,
-    subtree: true
-  });
-})();
+}).observe(document, { childList: true, subtree: true });
