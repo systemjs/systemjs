29cd4ed9cb339f967b989425040d45d59ed3914b
Damien Maillard <damien.maillard@dailymotion.com>
2020-09-13 19:26:45 +0200

    dumb commit

--- src/err-msg.js
+++ src/err-msg.js
@@ -1,6 +1,14 @@
 export function errMsg(errCode, msg) {
   if (process.env.SYSTEM_PRODUCTION)
-    return (msg || "") + " (SystemJS https://git.io/JvFET#" + errCode + ")";
+    return (msg || "") + " (SysteJS https://git.io/JvFET#" + errCode + ")";
   else
-    return (msg || "") + " (SystemJS Error#" + errCode + " " + "https://git.io/JvFET#" + errCode + ")";
+    return (
+      (msg || "") +
+      " (SystemJS Error#" +
+      errCode +
+      " " +
+      "https://git.io/JvFET#" +
+      errCode +
+      ")"
+    );
 }
