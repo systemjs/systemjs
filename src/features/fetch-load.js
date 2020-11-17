45317bd73dc4f6f10242345ca04c69945bb96410
Guy Bedford <guybedford@gmail.com>
2020-11-16 16:58:41 -0800

    remove remappings

--- src/features/fetch-load.js
+++ src/features/fetch-load.js
@@ -29,12 +29,6 @@ systemJSPrototype.instantiate = function (url, parent) {
     if (!contentType || !jsContentTypeRegEx.test(contentType))
       throw Error(errMsg(4, process.env.SYSTEM_PRODUCTION ? contentType : 'Unknown Content-Type "' + contentType + '", loading ' + url + (parent ? ' from ' + parent : '')));
     return res.text().then(function (source) {
-      var sourceMappingIndex = source.lastIndexOf('//# sourceMappingURL=');
-      if (sourceMappingIndex > -1) {
-        var sourceMappingEnd = source.indexOf('\n', sourceMappingIndex);
-        var sourceMapping = source.slice(sourceMappingIndex, sourceMappingEnd > -1 ? sourceMappingEnd : undefined);
-        source += '\n//# sourceMappingURL=' + resolveUrl(sourceMapping.slice(21), url);
-      }
       if (source.indexOf('//# sourceURL=') < 0)
         source += '\n//# sourceURL=' + url;
       (0, eval)(source);
