47408d477600b712cd0b0d0e057edd5c3d4f6264
Guy Bedford <guybedford@gmail.com>
2020-11-16 17:03:32 -0800

    Only add sourceURL if not present and remove Firefox workaround (#2279)
    * only add sourceURL if not present
    
    * remove remappings
    
    * remove import

--- src/features/fetch-load.js
+++ src/features/fetch-load.js
@@ -1,5 +1,4 @@
 import { errMsg } from '../err-msg.js';
-import { resolveUrl } from '../common.js';
 import { importMap } from '../features/import-maps.js';
 import { systemJSPrototype } from '../system-core.js';
 
@@ -29,13 +28,8 @@ systemJSPrototype.instantiate = function (url, parent) {
     if (!contentType || !jsContentTypeRegEx.test(contentType))
       throw Error(errMsg(4, process.env.SYSTEM_PRODUCTION ? contentType : 'Unknown Content-Type "' + contentType + '", loading ' + url + (parent ? ' from ' + parent : '')));
     return res.text().then(function (source) {
-      var sourceMappingIndex = source.lastIndexOf('//# sourceMappingURL=');
-      if (sourceMappingIndex > -1) {
-        var sourceMappingEnd = source.indexOf('\n', sourceMappingIndex);
-        var sourceMapping = source.slice(sourceMappingIndex, sourceMappingEnd > -1 ? sourceMappingEnd : undefined);
-        source += '\n//# sourceMappingURL=' + resolveUrl(sourceMapping.slice(21), url);
-      }
-      source += '\n//# sourceURL=' + url;
+      if (source.indexOf('//# sourceURL=') < 0)
+        source += '\n//# sourceURL=' + url;
       (0, eval)(source);
       return loader.getRegister();
     });
