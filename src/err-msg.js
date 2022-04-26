c528549850dac160839857ce72fab8b2f1b88bcd
SukkaW <isukkaw@gmail.com>
2022-04-26 22:47:39 +0800

    refactor: replace all `git.io` within the error message

--- src/err-msg.js
+++ src/err-msg.js
@@ -1,6 +1,6 @@
 export function errMsg(errCode, msg) {
   if (process.env.SYSTEM_PRODUCTION)
-    return (msg || "") + " (SystemJS https://git.io/JvFET#" + errCode + ")";
+    return (msg || "") + " (SystemJS https://github.com/systemjs/systemjs/blob/main/docs/errors.md#" + errCode + ")";
   else
-    return (msg || "") + " (SystemJS Error#" + errCode + " " + "https://git.io/JvFET#" + errCode + ")";
+    return (msg || "") + " (SystemJS Error#" + errCode + " " + "https://github.com/systemjs/systemjs/blob/main/docs/errors.md#" + errCode + ")";
 }
