6570b11ab536e37db31ec6f57b940df31c0824f6
Todd Brannam <tbrannam@casualpenguin.com>
2025-05-16 09:17:34 -0400

    wip: const => var
    updated comment block

--- src/extras/context-resolve.js
+++ src/extras/context-resolve.js
@@ -1,21 +1,16 @@
-/*
- * SystemJS context.import.resolve as synchronous function
+/**
+ * Support for SystemJS createContext to have a synchronous resolve method
+ * that matches the behavior of the native `import.meta.resolve` in Node 18+. 
+ * see: https://nodejs.org/api/esm.html#importmetaresolvespecifier
+ * Note Node: v20.0.0, v18.19.0 | This API now returns a string synchronously instead of a Promise.
  */
-
-
-// SystemJS defines dynamic `import.meta.resolve` as being asynchronous, but this is not the case in node 18+
-// it also is different than System.resolve's behavior, which is synchronous
-// see: https://nodejs.org/api/esm.html#importmetaresolvespecifier
-// Note Node: v20.0.0, v18.19.0	| This API now returns a string synchronously instead of a Promise.
 
 (function (global) {
   var systemJSPrototype = global.System.constructor.prototype;
-
   var createContext = systemJSPrototype.createContext;
-
   systemJSPrototype.createContext = function (parentId) {
-    const loader = this;
-    const context = createContext.call(this, parentId);
+    var loader = this;
+    var context = createContext.call(this, parentId);
     return Object.assign(context, {
       resolve: function (id, parentUrl) {
         return loader.resolve(id, parentUrl || parentId)
@@ -23,4 +18,3 @@
     });
   }
 })(typeof self !== 'undefined' ? self : global)
-
