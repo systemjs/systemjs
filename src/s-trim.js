e9f1e74cbd9436082f1d6af92aac3b290dd13fbd
Todd Brannam <tbrannam@casualpenguin.com>
2025-05-19 13:13:27 -0400

    feat: add additional browser flavor
    Add new flavor of systemjs for browser,  a package that excludes extras, but includes registry.

---
+++ src/s-trim.js
@@ -0,0 +1,7 @@
+import './features/script-load.js';
+import './features/fetch-load.js';
+import './features/resolve.js';
+import './features/import-maps.js';
+import './features/depcache.js';
+import './features/worker-load.js';
+import './features/registry.js';
